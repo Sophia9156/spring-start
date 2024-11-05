628fc9d2ba4e7c3d6964db6807a3e6a8a779b12d
Sophia9156 <suhyun9156@gmail.com>
2024-11-05 17:37:16 +0900

    feat: 저장

--- src/main/java/sophia/hellospring/controller/MemberController.java
+++ src/main/java/sophia/hellospring/controller/MemberController.java
@@ -2,6 +2,7 @@ package sophia.hellospring.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import sophia.hellospring.service.MemberService;
 
 @Controller
@@ -11,5 +12,10 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
     }
 }
