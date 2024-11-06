b19f12502ad1a614f35ab21248abb8d2437bcdaa
Sophia9156 <suhyun9156@gmail.com>
2024-11-06 10:57:46 +0900

    feat: 회원 등록 & 조회 기능 추가

--- src/main/java/sophia/hellospring/controller/MemberController.java
+++ src/main/java/sophia/hellospring/controller/MemberController.java
@@ -2,8 +2,13 @@ package sophia.hellospring.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import sophia.hellospring.domain.Member;
 import sophia.hellospring.service.MemberService;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -17,5 +22,22 @@ public class MemberController {
     @GetMapping("/members/new")
     public String createForm() {
         return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
---
+++ src/main/java/sophia/hellospring/controller/MemberForm.java
@@ -0,0 +1,10 @@
+package sophia.hellospring.controller;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MemberForm {
+    private String name;
+}
